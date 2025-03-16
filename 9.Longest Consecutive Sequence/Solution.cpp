6dd19f987cd94332283aff6ce43a8c31f3e56fdc
Priyank0777 <91007887+priyank0777@users.noreply.github.com>
2025-03-16 23:22:24 +0530

    Added CPP solution

---
+++ 9.Longest Consecutive Sequence/Solution.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
+        
+        for (int num : numSet) {
+            if (!numSet.count(num - 1)) {  // Check sequence start
+                int current = num;
+                int streak = 1;
+                
+                while (numSet.count(current + 1)) {  // Expand sequence
+                    current++;
+                    streak++;
+                }
+                
+                longest = max(longest, streak);
+            }
+        }
+        
+        return longest;
+    }
+};
+
+// Example usage
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 0, 1, 2};
+    cout << sol.longestConsecutive(nums);  // Output: 3
+    return 0;
+}
