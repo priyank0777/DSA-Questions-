4241fec8d84a88fa0e76619c081cdc96c7ce3593
Priyank0777 <91007887+priyank0777@users.noreply.github.com>
2025-03-16 23:28:03 +0530

    Added java solution

---
+++ 9.Longest Consecutive Sequence/Solution.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        
+        int longestStreak = 0;
+        
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {  // Sequence start check
+                int currentNum = num;
+                int currentStreak = 1;
+                
+                while (numSet.contains(currentNum + 1)) {  // Expand sequence
+                    currentNum++;
+                    currentStreak++;
+                }
+                
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        
+        return longestStreak;
+    }
+}
+
